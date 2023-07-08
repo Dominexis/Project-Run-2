078aaec8b04d9fb56f964d0a8dedf0de4a73105e
dominexis <dominexis9@gmail.com>
2023-07-08 18:10:20 -0500

    Edited Prime's plot's name and icon

--- Plot Function Generator.py
+++ Plot Function Generator.py
@@ -12,7 +12,7 @@ DATA_PACK_PATH = PROGRAM_PATH / "Data Packs" / "Project Run 2 Core"
 # namespace, y coordinate, icon, name, description
 NAMESPACES = {
     (-2,-6): ("eosand", 0),
-    (-2,-1): ("time_stream", 0, "minecraft:clock", "Time Stream", "You've arrived at Time Labs Universe \"Time Stream\". Safe travels and don't die!"),
+    (-2,-1): ("time_stream", 0, "minecraft:clock", "Time Stream", "You've arrived at Time Labs Universe \"Time Stream\". Safe travels and don't get stuck in the time stream!"),
     (-1,-3): ("itspungpond98", 0, "minecraft:verdant_froglight", "The Froglight Town", "Find the froglights by following the trails, and exit through the gates."),
     (-1,5): ("funkytoc_moon", 1),
     (1,3): ("theswagunicorn", 0),
