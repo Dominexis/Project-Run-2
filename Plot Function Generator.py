50744cc962daac4a26f694749436af58de06c6ec
dominexis <dominexis9@gmail.com>
2023-08-04 22:09:36 -0500

    Applied Anikey Chang's advancement

--- Plot Function Generator.py
+++ Plot Function Generator.py
@@ -11,7 +11,7 @@ PROGRAM_PATH = Path(__file__).parent
 DATA_PACK_PATH = PROGRAM_PATH / "Data Packs" / "Project Run 2 Core"
 # namespace, y coordinate, icon, name, description
 NAMESPACES = {
-    (-3,0): ("anikey_plot", 4),
+    (-3,0): ("anikey_plot", 4, "minecraft:white_glazed_terracotta", "Mirror Travel", "Jumping in a Gothic building, try to go though it!"),
     (-3,1): ("aljaz", 0),
     (-3,4): ("zombie1111", 0),
     (-3,5): ("simondmc", 2, "minecraft:big_dripleaf", "Drip Jump", "Leaf us a review!"),
